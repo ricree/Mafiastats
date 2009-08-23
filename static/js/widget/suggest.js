558c52de10fe73b7422ea96db2e6fc34fb02d77c
ricree <ricree@gmail.com>
2009-08-22 23:03:06 -0600

    fixed error in autosuggest js that only pointed to dev machine

--- static/js/widget/suggest.js
+++ static/js/widget/suggest.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	$(".AutoSuggestBox").jsonSuggest("http://localhost:8000/player/name_lookup/",{wildCard:'*',maxResults:10,ajaxResults:true,useJQueryAjax:true,JQueryAjaxParam : "text"})
+	$(".AutoSuggestBox").jsonSuggest("/player/name_lookup/",{wildCard:'*',maxResults:10,ajaxResults:true,useJQueryAjax:true,JQueryAjaxParam : "text"})
 //	$(".AutoSuggestBox").jsonSuggest(testData.fruits);
 		
 	});
