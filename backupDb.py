ec2d7437a7e936ac0b40b041ca329665e179d744
ricree <ricree@gmail.com>
2009-10-09 02:58:18 -0600

    changes to backup code

--- backupDb.py
+++ backupDb.py
@@ -6,10 +6,9 @@ except ImportError:
 	BACKUP_DIRECTORY = "."
 current = datetime.datetime.now()
 filename = "%s/auto_backup_%s_%s_%s.json"%(BACKUP_DIRECTORY,current.day,current.month,current.year)
-command = "./manage.py dumpdata > %s"%filename
+command = "~/django_projects/Mafiastats/manage.py dumpdata > %s"%filename
 try:
 	os.system(command)
 	print "Backed up data on %s" % (current.strftime("%A %d %B %Y"))
 except Exception as e:
 	print "Backup failed with message: ",e.message
-	
