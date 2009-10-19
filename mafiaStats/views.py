0594ae454d94208d859ea26473ced0ebed0aa8c7
ricree <ricree@gmail.com>
2009-10-19 16:26:44 -0500

    role links now open the role description when clicked.

--- mafiaStats/views.py
+++ mafiaStats/views.py
@@ -113,8 +113,9 @@ def game(request, game_id):
 	numWinners =teams.filter(won=True).count()
 	teams=[(team,team.players.all().order_by('name')) for team in teams]
 	roles=dict(((role.player.name,role) for role in Role.objects.filter(game=game)))
+	roleOrder = dict(((role[1].pk,index) for role,index in zip(roles.items(),range(len(roles)))))
 	can_edit = request.user.has_perm('mafiaStats.game')
-	return render_to_response('game.html', {'can_edit':can_edit,'game':game, 'teams':teams, 'num_players' : numPlayers, 'length':length, 'next':reverse('mafiastats_game',args=[int(game_id)]),'players':players,'winners':winners,'roles':roles},context_instance=RequestContext(request))
+	return render_to_response('game.html', {'can_edit':can_edit,'game':game, 'teams':teams, 'num_players' : numPlayers, 'roleOrder':roleOrder,'length':length, 'next':reverse('mafiastats_game',args=[int(game_id)]),'players':players,'winners':winners,'roles':roles},context_instance=RequestContext(request))
 
 def player(request,player_id):
 	player = get_object_or_404(Player, pk=player_id)
